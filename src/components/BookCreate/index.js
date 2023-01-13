9fba8b3f5a5adc8d2a7528a3ca9f5c27f1ce0f94
Kostadinos01 <kostasigna@yahoo.gr>
2023-01-13 08:32:40 +0200

    Commit 12/01/2023

---
+++ src/components/BookCreate/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+
+import { useState } from "react";
+
+export default function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onCreate(title);
+
+    setTitle("");
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label> Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create!!</button>
+      </form>
+    </div>
+  );
+}
